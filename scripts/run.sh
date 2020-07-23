be8cbf6d63d783122934e227b2bd4d0a1e500f97
Mike Merrill <mikeam@cs.washington.edu>
2020-07-23 11:58:55 -0700

    Configure to work for mikeam

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,27 +10,27 @@
 
 # setup
 source ~/.bashrc
-export GIT_SSH_COMMAND='ssh -i ~/.ssh/robot_id_rsa'
+# export GIT_SSH_COMMAND='ssh -i ~/.ssh/robot_id_rsa'
 
 # setup 1: update self (ensure running latest version of backend). note that we
 # add "1> ~/.cronlog 2>&1" to redirect stderr to stdout and then redirect
 # stdout to .cronlog. the first command creates a fresh .cronlog by doing > and
 # the next ones append to it with >>.
-cd ~/repos/nlp-corpora-backend/ 1> ~/.cronlog 2>&1
+cd ~/bdatasets-backend 1> ~/.cronlog 2>&1
 git pull --rebase origin master 1>> ~/.cronlog 2>&1
 
 # step 2: run crawler
-pyenv activate nlp-corpora 1>> ~/.cronlog 2>&1
+conda activate bdatasets 1>> ~/.cronlog 2>&1
 python check.py \
-    --directory /projects/nlp-corpora/ \
-    --out-file ~/repos/nlp-corpora/README.md \
-    --log-file ~/repos/nlp-corpora/BUILD.txt \
-    --doc-dir ~/repos/nlp-corpora/doc \
-    --plot-dest ~/repos/nlp-corpora/disk-usage.svg
-pyenv deactivate
+    --directory /projects/bdata/datasets \
+    --out-file ~/bdatasets/README.md \
+    --log-file ~/bdatasets/BUILD.txt \
+    --doc-dir ~/bdatasets/doc \
+    --plot-dest ~/bdatasets/disk-usage.svg
+conda deactivate
 
 # setp 3: push updated doc
-cd ~/repos/nlp-corpora/
+cd ~/bdatasets/
 git add .
 git commit -m "update `date '+%m/%d/%Y %H:%M:%S'`" 1>> ~/.cronlog 2>&1
 git pull --regbase origin master 1>> ~/.cronlog 2>&1
